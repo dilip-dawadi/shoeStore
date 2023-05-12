0a82c4db6ff0476a13e61b95a6cbebed007ee2d6
dilip-zan <zanzerdawadi123@gmail.com>
2023-05-12 23:42:59 +0545

    Cookie Update

--- server/controller/user.js
+++ server/controller/user.js
@@ -22,7 +22,7 @@ const generateSessionToken = (data, res) => {
     expires: new Date(
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
-    httpOnly: false,
+    httpOnly: process.env.NODE_ENV === "production" ? true : false,
     secure: process.env.NODE_ENV === "production" ? true : false,
     sameSite: "strict",
   });
