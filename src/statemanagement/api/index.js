b5614287dac1dac2ef80a55af41c6565b44c430b
dilip-zan <zanzerdawadi123@gmail.com>
2023-05-13 08:26:06 +0545

    Part_4_Shoe_Store Final

--- src/statemanagement/api/index.js
+++ src/statemanagement/api/index.js
@@ -1,20 +1,3 @@
-// fetch data from api
-import axios from "axios";
-let baseURL;
-// check if not production
-if (process.env.NODE_ENV !== "production") {
-  baseURL = process.env.REACT_APP_BASE_URL_LOCAL;
-} else {
-  baseURL = process.env.REACT_APP_BASE_URL;
-}
-axios.defaults.withCredentials = true;
-const API = axios.create({ baseURL });
-API.interceptors.request.use(
-  (req) => {
-    return req;
-  },
-  (error) => {
-    return Promise.reject(error.message);
-  }
-);
-export default API;
+// Part_4_Shoe_Store is not officially out yet,
+//but you can mail me to get the source code.
+// mail id: check my youtube channel about section.
